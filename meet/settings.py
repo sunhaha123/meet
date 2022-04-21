7060a16b1b0b238a14085405b1a07a456be4bf19
sunhaha123 <sunjh007@163.com>
2022-04-21 20:56:00 +0800

    core-head

--- meet/settings.py
+++ meet/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 import sys
+import corsheaders
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -39,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'index',
-    'corsheaders'
+    'corsheaders',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -83,6 +84,7 @@ CORS_ALLOW_HEADERS = (
  'x-csrftoken',
  'x-requested-with',
  'Pragma',
+ 'content-type',
 )
 #部署到云服务上必备
 ALLOWED_HOSTS = ['*']
